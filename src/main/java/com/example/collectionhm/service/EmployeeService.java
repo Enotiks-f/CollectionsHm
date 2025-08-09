f38212bf16085f1cd21cf28d2841a8bb5781a1ec
Titov Ilya <titov1608@gmail.com>
2025-08-09 16:20:20 +0300

    add Exception for Service

--- src/main/java/com/example/collectionhm/service/EmployeeService.java
+++ src/main/java/com/example/collectionhm/service/EmployeeService.java
@@ -1,6 +1,9 @@
 package com.example.collectionhm.service;
 
 import com.example.collectionhm.Employee;
+import com.example.collectionhm.exception.EmployeeAlreadyAddedException;
+import com.example.collectionhm.exception.EmployeeNotFoundException;
+import com.example.collectionhm.exception.EmployeeStorageIsFullException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -20,20 +23,36 @@ public class EmployeeService {
     // метод добавления сотридника в лист
     public void addEmployee(String firstName, String lastName) {
         Employee employee = new Employee(firstName, lastName);
+
+        // Проверка на макс. кол-во сотрудников
+        if  (employees.size() >= maxEmployees) {
+            throw new EmployeeStorageIsFullException("Кол-во сотрудников превысило максимальное значение: " + maxEmployees);
+        }
+
+        // Проверка на дубликат
+        if (employees.contains(employee)) {
+            throw new EmployeeAlreadyAddedException("Такой сотрудник уже есть");
+        }
         employees.add(employee);
+
     }
 
     // метод удаления соттрудника из листа
     public String deleteEmployee(String firstName, String lastName) {
         boolean a = employees.remove(new Employee(firstName, lastName));
         if (!a){
-            return "Сотрудник " +  firstName + " " + lastName + "не найден";
+            throw new EmployeeNotFoundException("Сотрудник " +  firstName + " " + lastName + "не найден");
         }
         return "Сотрудник " +  firstName + " " + lastName + "был удален";
     }
 
+    // метод получение сотрудника по индексу
     public Employee getEmployee(int index) {
-        return employees.get(index);
+        try {
+            return employees.get(index);
+        }catch (EmployeeNotFoundException e){
+            throw new EmployeeNotFoundException("Cотрудник с " + index + " индексом не найден");
+        }
     }
 
 }
